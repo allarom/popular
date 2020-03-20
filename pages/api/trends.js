3dd1ef35cae7ed82b7346538cdfa42bd7b449c11
Alla Roman <alla.roman.mail@gmail.com>
2020-03-20 19:15:03 +0100

    try

--- pages/api/trends.js
+++ pages/api/trends.js
@@ -2,18 +2,22 @@ import googleTrends from 'google-trends-api'
 import Cors from 'cors'
 
 
-module.exports = async(req, res) => {
+module.exports = (req, res) => {
   const cors = Cors({
     methods: ['GET', 'HEAD', 'POST'],
   })
   console.log("req trends", req)
   console.log("res trends", res)
-  googleTrends.dailyTrends({ geo: 'CH' })
-  .then(function(results){
-    res.status(200).end(results)
-  })
-  .catch(function(err){
-    console.error('Oh no there was an error', err);
-  });
+
+    const { name = 'World' } = req.query
+    res.status(200).send(`Hello ${name}!`)
+
+  // googleTrends.dailyTrends({ geo: 'CH' })
+  // .then(function(results){
+  //   res.status(200).end(results)
+  // })
+  // .catch(function(err){
+  //   console.error('Oh no there was an error', err);
+  // });
 
 }
