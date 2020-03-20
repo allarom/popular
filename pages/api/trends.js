3584b794b9cd356cf55cf067576d96a8feed68c6
Alla Roman <alla.roman.mail@gmail.com>
2020-03-20 20:21:18 +0100

    try

--- pages/api/trends.js
+++ pages/api/trends.js
@@ -1,7 +1,6 @@
 const googleTrends = require('google-trends-api')
 // import Cors from 'cors'
 
-
 module.exports = (req, res) => {
   // const cors = Cors({
   //   methods: ['GET', 'HEAD', 'POST'],
@@ -12,7 +11,7 @@ module.exports = (req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({ name: 'John Doe' }))
-  
+
   // googleTrends.dailyTrends({ geo: 'CH' })
   // .then(function(results){
   //   res.status(200).end(results)
